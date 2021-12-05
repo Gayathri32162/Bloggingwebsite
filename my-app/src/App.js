8aef7d477c49397238de31e6a109ccefdfda72d4
Gayathri32162 <gayathribalamurugan82@gmail.com>
2021-12-05 16:34:26 +0530

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>
+        this is a blogging website
+      </h1>
+    </div>
+  )
+}
+
+export default App
